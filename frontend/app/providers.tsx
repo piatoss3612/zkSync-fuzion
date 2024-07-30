fa70cc97948999695e1024467643b9de58f4ff48
piatoss <crewe1746@naver.com>
2024-07-30 20:55:22 +0900

    feat: connect wallet button with rainbowkit

---
+++ frontend/app/providers.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { zkSync, zkSyncSepoliaTestnet } from "viem/zksync";
+import { WagmiProvider } from "wagmi";
+import "@rainbow-me/rainbowkit/styles.css";
+
+const config = getDefaultConfig({
+  appName: "Fuzion",
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "",
+  chains: [zkSync, zkSyncSepoliaTestnet],
+  ssr: true,
+});
+
+const queryClient = new QueryClient();
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  const [mounted, setMounted] = useState<boolean>(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <ChakraProvider>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitProvider coolMode initialChain={zkSyncSepoliaTestnet}>
+            {mounted && children}
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </ChakraProvider>
+  );
+};
+
+export default Providers;
