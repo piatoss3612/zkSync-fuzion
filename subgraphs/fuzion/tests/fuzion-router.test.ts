d16c65dc734445ca90e702861688bb80bd6c13b5
piatoss <crewe1746@naver.com>
2024-08-03 20:30:09 +0900

    feat: deploy fuzion subgraph

--- subgraphs/fuzion/tests/fuzion-router.test.ts
+++ subgraphs/fuzion/tests/fuzion-router.test.ts
@@ -4,13 +4,13 @@ import {
   test,
   clearStore,
   beforeAll,
-  afterAll
-} from "matchstick-as/assembly/index"
-import { Address } from "@graphprotocol/graph-ts"
-import { OwnershipTransferred } from "../generated/schema"
-import { OwnershipTransferred as OwnershipTransferredEvent } from "../generated/fuzion_router/fuzion_router"
-import { handleOwnershipTransferred } from "../src/fuzion-router"
-import { createOwnershipTransferredEvent } from "./fuzion-router-utils"
+  afterAll,
+} from "matchstick-as";
+import { Address } from "@graphprotocol/graph-ts";
+import { OwnershipTransferred } from "../generated/schema";
+import { OwnershipTransferred as OwnershipTransferredEvent } from "../generated/fuzion_router/fuzion_router";
+import { handleOwnershipTransferred } from "../src/fuzion-router";
+import { createOwnershipTransferredEvent } from "./fuzion-router-utils";
 
 // Tests structure (matchstick-as >=0.5.0)
 // https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
@@ -19,26 +19,26 @@ describe("Describe entity assertions", () => {
   beforeAll(() => {
     let previousOwner = Address.fromString(
       "0x0000000000000000000000000000000000000001"
-    )
+    );
     let newOwner = Address.fromString(
       "0x0000000000000000000000000000000000000001"
-    )
+    );
     let newOwnershipTransferredEvent = createOwnershipTransferredEvent(
       previousOwner,
       newOwner
-    )
-    handleOwnershipTransferred(newOwnershipTransferredEvent)
-  })
+    );
+    handleOwnershipTransferred(newOwnershipTransferredEvent);
+  });
 
   afterAll(() => {
-    clearStore()
-  })
+    clearStore();
+  });
 
   // For more test scenarios, see:
   // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
 
   test("OwnershipTransferred created and stored", () => {
-    assert.entityCount("OwnershipTransferred", 1)
+    assert.entityCount("OwnershipTransferred", 1);
 
     // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
     assert.fieldEquals(
@@ -46,15 +46,15 @@ describe("Describe entity assertions", () => {
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "previousOwner",
       "0x0000000000000000000000000000000000000001"
-    )
+    );
     assert.fieldEquals(
       "OwnershipTransferred",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "newOwner",
       "0x0000000000000000000000000000000000000001"
-    )
+    );
 
     // More assert options:
     // https://thegraph.com/docs/en/developer/matchstick/#asserts
-  })
-})
+  });
+});
