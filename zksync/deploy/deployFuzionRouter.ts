1a0860bd8e1572454c7cdd9e48ffeb74f90131f6
piatoss <crewe1746@naver.com>
2024-08-14 17:54:24 +0900

    feat: factory deployment script

--- zksync/deploy/deployFuzionRouter.ts
+++ zksync/deploy/deployFuzionRouter.ts
@@ -2,16 +2,9 @@ import { deployContract, getWallet } from "./utils";
 
 export default async function () {
   const wallet = getWallet();
+  const paymasterFactoryAddress = "0x111A479Ac2bA70026dc6783cfB5321076Af9076F";
 
   const contractArtifactName = "FuzionRouter";
-  const constructorArguments = [wallet.address];
-  const contract = await deployContract(
-    contractArtifactName,
-    constructorArguments
-  );
-
-  const tx = await contract.setPaymasterFactory(
-    "0x1cBa868B876cB00140a43895246559b2e5E129c9"
-  );
-  await tx.wait();
+  const constructorArguments = [paymasterFactoryAddress, wallet.address];
+  await deployContract(contractArtifactName, constructorArguments);
 }
--- zksync/deploy/deployGaslessPaymasterFactory.ts
+++
@@ -1,9 +0,0 @@
-import { deployFactoryContract } from "./utils";
-import { ethers } from "ethers";
-
-export default async function () {
-  const factoryName = "GaslessPaymasterFactory";
-  const deps = ["GaslessPaymaster"];
-
-  await deployFactoryContract(factoryName, [], deps);
-}
