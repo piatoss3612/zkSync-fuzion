885411619e32cd2e78d1c95017fdcbc042b073fb
piatoss <crewe1746@naver.com>
2024-08-15 16:54:36 +0900

    docs: annotations on interfaces

--- zksync/contracts/interfaces/IFuzionRouter.sol
+++ zksync/contracts/interfaces/IFuzionRouter.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.24;
 
 import {IModule, ModuleType} from "./IModule.sol";
 
+/// @title Fuzion Router Interface
+/// @author piatoss3612
+/// @dev Interface for the Fuzion Router contract
 interface IFuzionRouter {
     error FuzionRouter__ZeroAddress();
     error FuzionRouter__PaymasterFactoryNotAvailable();
@@ -12,7 +15,15 @@ interface IFuzionRouter {
     event PaymasterCreated(address indexed paymaster, address indexed owner, string name);
     event ModuleRegistered(address indexed module, ModuleType moduleType, string name);
 
+    /// @dev Returns the factory address
+    /// @return The factory address
     function factory() external view returns (address);
+    /// @dev Creates a paymaster with the given parameters
+    /// @param _salt The salt for the paymaster (create2 address)
+    /// @param _owner The owner of the paymaster
+    /// @param _feeTo The fee to address of the paymaster
+    /// @param _alias The alias name of the paymaster (to index the paymaster with the subgraph)
+    /// @param _initData The initialization data for the paymaster
     function createPaymaster(
         bytes32 _salt,
         address _owner,
@@ -20,5 +31,9 @@ interface IFuzionRouter {
         string calldata _alias,
         bytes calldata _initData
     ) external payable;
+    /// @dev Registers a module with the given address (only one time registration)
+    /// @param _module The address of the module to register
     function registerModule(address _module) external;
+
+    // TODO: rating system for community modules
 }
--- zksync/contracts/interfaces/IModule.sol
+++ zksync/contracts/interfaces/IModule.sol
@@ -5,28 +5,58 @@ import {ExecutionResult} from "@matterlabs/zksync-contracts/l2/system-contracts/
 import {Transaction} from "@matterlabs/zksync-contracts/l2/system-contracts/libraries/TransactionHelper.sol";
 import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
+/// @title Module Type Enum
+/// @dev Validator, Payport, Hook module types
+///      Validator: Validates the transaction
+///      Payport: Prepares the payment and refund data
+///      Hook: Pre-checks and post-checks the transaction
 enum ModuleType {
     Validator,
     Payport,
     Hook
 }
 
+/// @title Module Metadata Struct
+/// @dev Metadata of the module
+///      moduleType: Type of the module
+///      name: Name of the module
+///      version: Version of the module
+///      author: Author of the module
+///      installDataSignature: Signature of the install data
 struct ModuleMetadata {
-    ModuleType moduleType; // Type of the module.
-    string name; // Name of the module.
-    string version; // Version of the module.
-    string author; // Author of the module.
-    string installDataSignature; // Signature of the install data. (I think it would be better to use (type, name) array instead of just a string)
+    ModuleType moduleType;
+    string name;
+    string version;
+    string author;
+    string installDataSignature;
 }
 
+/// @title Module Interface
+/// @dev Interface for the module used in the Fuzion Paymaster
 interface IModule is IERC165 {
-    function onInstall(bytes calldata data) external;
+    /// @dev Initializes the module with the given initialization data, called by the paymaster
+    /// @param data The initialization data for the module
+    function onInstall(bytes calldata data) external payable;
+    /// @dev Uninstalls the module with the given data, called by the paymaster
+    /// @param data The uninstallation data for the module
     function onUninstall(bytes calldata data) external;
+    /// @dev Returns the metadata of the module
+    /// @return Metadata of the module
     function metadata() external view returns (ModuleMetadata memory);
+    /// @dev Checks whether the module is of the given type
+    /// @param _moduleType The type of the module to check
     function isModuleType(ModuleType _moduleType) external view returns (bool);
+    /// @dev Checks whether the module is installed for the given account, mapped by the account address
+    /// @param _account The account to check
     function isInstalled(address _account) external view returns (bool);
 }
 
+/// @title Module Init Data Struct
+/// @dev Initialization data for the module used in the Fuzion Paymaster `initialize` function
+///      moduleType: Type of the module
+///      isDefault: Whether the module is default
+///      module: Address of the module
+///      initData: Initialization data for the module
 struct ModuleInitData {
     ModuleType moduleType;
     bool isDefault;
@@ -34,33 +64,71 @@ struct ModuleInitData {
     bytes initData;
 }
 
+/// @title Validator Module Interface
+/// @dev Interface for the validator module used in the Fuzion Paymaster
 interface IValidator is IModule {
+    /// @dev Validates the transaction with the given transaction hash, suggested signed hash, and transaction
+    /// @param _txHash The hash of the transaction
+    /// @param _suggestedSignedHash The hash of the transaction that is signed by an EOA
+    /// @param _transaction The transaction itself.
+    /// @return isValid Whether the transaction is valid
     function validateTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction calldata _transaction)
         external
         view
         returns (bool isValid);
 }
 
+/// @title Prepare Payment Data Struct for Payport Module
+/// @dev Contains the data for payment preparation in the Payport module
+///      paymasterInputSelector: Selector of the IPaymasterFlow method (for double check just in case)
+///      from: Address to transfer from
+///      token: Not zero if token transfer is required
+///      requiredETH: Required amount of ETH to transfer to the Bootloader
+///      requiredToken: Required amount of token
+///      extraData: Reserved for future use
 struct PreparePaymentData {
-    bytes4 paymasterInputSelector; // Selector of the IPaymasterFlow method (for double check just in case).
-    address from; // Address to transfer from.
-    address token; // Not zero if token transfer is required.
-    uint96 requiredETH; // Required amount of ETH to transfer to the Bootloader. (fee might not greater than 1 ETH, so uint96 is enough)
-    uint256 requiredToken; // Required amount of token.
-    bytes extraData; // Reserved for future use.
+    bytes4 paymasterInputSelector;
+    address from;
+    address token;
+    uint96 requiredETH;
+    uint256 requiredToken;
+    bytes extraData;
 }
 
+/// @title Prepare Refund Data Struct for Payport Module
+/// @dev Contains the data for refund preparation in the Payport module
+///      token: Not zero if token transfer is required
+///      to: Address to transfer refund to
+///      fee: Fee to charge from the refund (to cover the gas cost paymaster spent for additional checks)
+///      amount: Amount of token to refund excluding fee
 struct PrepareRefundData {
-    address token; // Not zero if token transfer is required.
-    address to; // Address to transfer refund to.
-    uint96 fee; // Fee to charge from the refund (to cover the gas cost paymaster spent for additional checks).
-    uint256 amount; // Amount of token to refund excluding fee.
+    address token;
+    address to;
+    uint96 fee;
+    uint256 amount;
 }
 
+/// @title Payport Module Interface
+/// @dev Interface for the payport module used in the Fuzion Paymaster
 interface IPayport is IModule {
+    /// @dev Prepares the payment data for the transaction
+    ///      as the module is called externally, payment is not executed in this function
+    ///      only the data is prepared for the payment and actual payment is executed by the paymaster
+    /// @param _transaction The transaction to prepare the payment data for
+    /// @return paymentData The payment data for the transaction
     function preparePayment(Transaction calldata _transaction)
         external
         returns (PreparePaymentData memory paymentData);
+    /// @dev Prepares the refund data for the transaction
+    ///      as the module is called externally, refund is not executed in this function
+    ///      only the data is prepared for the refund and actual refund is executed by the paymaster
+    /// @param _transaction The transaction to prepare the refund data for
+    /// @param _paymentData The payment data for the transaction
+    /// @param _txHash The hash of the transaction
+    /// @param _suggestedSignedHash The hash of the transaction that is signed by an EOA
+    /// @param _txResult The result of the transaction execution
+    /// @param _maxRefundedGas The maximum gas to refund
+    /// @return refundData The refund data for the transaction
     function prepareRefund(
         Transaction calldata _transaction,
         PreparePaymentData calldata _paymentData,
@@ -71,10 +139,19 @@ interface IPayport is IModule {
     ) external returns (PrepareRefundData memory refundData);
 }
 
+/// @title Hook Module Interface
+/// @dev Interface for the hook module used in the Fuzion Paymaster
+///      Hook module is used for pre-checks and post-checks of the transaction
 interface IHook is IModule {
+    /// @dev Pre-checks the transaction with the given transaction hash, suggested signed hash, and transaction
+    /// @param _txHash The hash of the transaction
+    /// @param _suggestedSignedHash The hash of the transaction that is signed by an EOA
+    /// @param _transaction The transaction itself.
+    /// @return hookContext The context for the hook
     function preCheck(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction calldata _transaction)
         external
         returns (bytes memory hookContext);
-
+    /// @dev Post-checks the transaction with the given hook context
+    /// @param hookContext The context for the hook
     function postCheck(bytes calldata hookContext) external;
 }
